df8a11fe1807b2dbfa6e3bcbb829c0f6d8c4e484
davehasagithub <dave@terrian.com>
2024-03-10 17:39:59 -0400

    Modify replacement approach

--- daveml.sh
+++ daveml.sh
@@ -3,22 +3,32 @@ file="${1:-/dev/stdin}"
 if [ -n "$TERM" ]; then
   ESC="\e"
 
+  # color codes are <foreground><bold|X><background|X>
   if [[ "$TERM" == *color* ]]; then
-    BBG="${ESC}[37;44;1m"
-    GGG="${ESC}[32m"
-    CCC="${ESC}[36m"
-    BBF="${ESC}[31;1m"
-    CLR="${ESC}[0m"
+    WBB="${ESC}[37;44;1m" # fg:white bold bg:blue
+    WBX="${ESC}[37;1m"    # fg:white bold
+    GXX="${ESC}[32m"      # fg:green
+    CXX="${ESC}[36m"      # fg:cyan
+    RBX="${ESC}[31;1m"    # fg:red bold
+    YXX="${ESC}[33m"      # fg:yellow
+    YBX="${ESC}[33;1m"    # fg:yellow bold
+    CLR="${ESC}[0m"       # reset
   fi
 
   perl -pe "
-    s/^(.*?)<BBG>/\$1$BBG/g;
-    s/^(.*?)<GGG>/\$1$GGG/g;
-    s/^(.*?)<CCC>/\$1$CCC/g;
-    s/^(.*?)<BBF>/\$1$BBF/g;
-    s/^(.*?)<CLR>/\$1$CLR/g;
-    s/^(.*?${ESC}.*)$/\$1$CLR/g;
-    s/^/| /g;
+    s/<WBB>/$CLR$WBB/g;
+    s/<WBX>/$CLR$WBX/g;
+    s/<GXX>/$CLR$GXX/g;
+    s/<CXX>/$CLR$CXX/g;
+    s/<RBX>/$CLR$RBX/g;
+    s/<YXX>/$CLR$YXX/g;
+    s/<YBX>/$CLR$YBX/g;
+    s/<CLR>/$CLR/g;
   " \
-  "$file"
+  "$file" | \
+
+    perl -pe "
+      s/^(.*?${ESC}.*)$/\$1$CLR/g;
+      s/^/| /g;
+    "
 fi
